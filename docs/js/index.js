1a9020b2a9742b48adb08d956bfd3618d158f3a9
Mina-Kiko <kiko.yuch@gmail.com>
2019-09-20 01:18:35 +0900

    新規作成

---
+++ docs/js/index.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+    $('.slider').slick({
+        autoplay:true,
+        autoplaySpeed:3000,
+        dots:true,
+        slidesToShow:3,
+        slidesToScroll:1,
+        dots:true
+    });
+});
+
